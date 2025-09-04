416cf94c489830671d9fff9e79706ae40b394664
Dmitri Manajev <dmitri.manajev@protonmail.com>
2025-09-04 13:13:53 +0000

    Added rviz config and launch files to work with my_rb1_robot.urdf

--- my_rb1_description/setup.py
+++ my_rb1_description/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'my_rb1_description'
@@ -10,13 +12,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='user',
     maintainer_email='dmitri.manajev@protonmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Robot description package for my RB1, including URDF models and launch files.',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
